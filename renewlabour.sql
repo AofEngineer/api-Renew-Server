869544cca972308be23f7cee57d9d23473b3d7fc
AofEngineer <124555693+AofEngineer@users.noreply.github.com>
2023-10-05 12:11:10 +0700

    commit for up to server

--- renewlabour.sql
+++ renewlabour.sql
@@ -1,5 +1,16 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+07:00";
+
+
+CREATE TABLE `member_group` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `member_group` (`id`) VALUES
+(1);
+
 CREATE TABLE `branch` (
   `branch_id` int(100) NOT NULL,
   `branch_no` varchar(100) NOT NULL,
@@ -7,6 +18,7 @@ CREATE TABLE `branch` (
   `member_group` int(11) NOT NULL,
   `company_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 CREATE TABLE `company` (
   `cpn_id` int(11) NOT NULL,
   `cpn_n` varchar(100) DEFAULT NULL,
@@ -27,6 +39,7 @@ CREATE TABLE `company` (
   `member_group` int(11) NOT NULL,
   `branch_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 CREATE TABLE `members` (
   `mem_id` int(11) NOT NULL,
   `member_name` varchar(50) DEFAULT NULL,
@@ -37,23 +50,18 @@ CREATE TABLE `members` (
   `email` varchar(255) DEFAULT NULL,
   `tel` varchar(10) DEFAULT NULL,
   `activation_code` varchar(50) DEFAULT NULL,
-  `piority` int(2) NOT NULL,
   `refreshtoken` varchar(255) DEFAULT NULL,
-  `company_id` int(11) DEFAULT NULL
+  `company_id` int(11) DEFAULT NULL,
+  `m_read` tinyint(1) NOT NULL,
+  `m_add` tinyint(1) NOT NULL,
+  `m_edit` tinyint(1) NOT NULL,
+  `m_del` int(11) NOT NULL,
+  `m_admin` tinyint(1) NOT NULL,
+  `m_root` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-CREATE TABLE `member_group` (
-  `id` int(11) NOT NULL,
-  `company_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-CREATE TABLE `permission` (
-  `per_id` int(11) NOT NULL,
-  `readpermis` tinyint(1) NOT NULL,
-  `addpermis` tinyint(1) NOT NULL,
-  `editpermis` tinyint(1) NOT NULL,
-  `delpermis` tinyint(1) NOT NULL,
-  `rootadmin` tinyint(1) DEFAULT NULL,
-  `admin` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
 CREATE TABLE `persons` (
   `person_id` int(11) NOT NULL,
   `outlanderNo` varchar(15) NOT NULL,
@@ -82,48 +90,59 @@ CREATE TABLE `persons` (
   `member_group` int(11) NOT NULL,
   `company_id` int(255) DEFAULT NULL,
   `picpath` varchar(255) DEFAULT NULL,
-  `nickname` varchar(25) DEFAULT NULL
+  `nickname` varchar(25) DEFAULT NULL,
+  `other` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `company` (`cpn_id`, `cpn_n`, `cpn_build`, `cpn_ad`, `cpn_fl`, `cpn_vill`, `cpn_room`, `cpn_moo`, `cpn_soi`, `cpn_st`, `cpn_coun`, `cpn_subdist`, `cpn_dist`, `cpn_prov`, `cpn_zip`, `c_iden`, `member_group`, `branch_id`) VALUES
+(0, 'ว่างงาน', NULL, '', NULL, NULL, NULL, NULL, NULL, NULL, '', '', '', '', '', '', 1, 0);
+
 ALTER TABLE `branch`
   ADD PRIMARY KEY (`branch_id`),
   ADD KEY `member_group` (`member_group`),
   ADD KEY `company_id` (`company_id`);
+
 ALTER TABLE `company`
   ADD PRIMARY KEY (`cpn_id`),
   ADD KEY `member_group` (`member_group`);
+
+
 ALTER TABLE `members`
   ADD PRIMARY KEY (`mem_id`),
-  ADD KEY `member_group` (`member_group`),
-  ADD KEY `piority` (`piority`);
+  ADD KEY `member_group` (`member_group`);
+
 ALTER TABLE `member_group`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `company_id` (`company_id`) USING BTREE;
-ALTER TABLE `permission`
-  ADD UNIQUE KEY `per_id` (`per_id`),
-  ADD KEY `per_id_2` (`per_id`);
+  ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `persons`
   ADD PRIMARY KEY (`person_id`),
   ADD KEY `member_group` (`member_group`),
   ADD KEY `company_id` (`company_id`);
+
 ALTER TABLE `branch`
-  MODIFY `branch_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `branch_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
 ALTER TABLE `company`
-  MODIFY `cpn_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `cpn_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
 ALTER TABLE `members`
-  MODIFY `mem_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `mem_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
 ALTER TABLE `member_group`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-ALTER TABLE `permission`
-  MODIFY `per_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
 ALTER TABLE `persons`
-  MODIFY `person_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `person_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
 ALTER TABLE `branch`
   ADD CONSTRAINT `branch_ibfk_1` FOREIGN KEY (`member_group`) REFERENCES `member_group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 ALTER TABLE `company`
   ADD CONSTRAINT `company_ibfk_1` FOREIGN KEY (`member_group`) REFERENCES `member_group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 ALTER TABLE `members`
-  ADD CONSTRAINT `members_ibfk_1` FOREIGN KEY (`member_group`) REFERENCES `member_group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `members_ibfk_2` FOREIGN KEY (`piority`) REFERENCES `permission` (`per_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `members_ibfk_1` FOREIGN KEY (`member_group`) REFERENCES `member_group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 ALTER TABLE `persons`
   ADD CONSTRAINT `persons_ibfk_3` FOREIGN KEY (`member_group`) REFERENCES `member_group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
